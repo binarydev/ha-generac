b748619327e00c11a51a9cec02d6549804ec38a4
Jose Santiago <jose@binary-dev.com>
2025-07-14 01:09:47 -0400

    Set up basic tests and moved local_test for the API call into a proper test we can optionally run.

---
+++ tests/test_config_flow.py
@@ -0,0 +1,41 @@
+"""Test the Generac config flow."""
+from unittest.mock import patch
+
+from homeassistant import config_entries, setup
+from homeassistant.core import HomeAssistant
+
+from custom_components.generac.const import DOMAIN
+
+
+async def test_form(hass: HomeAssistant) -> None:
+    """Test we get the form."""
+    await setup.async_setup_component(hass, "persistent_notification", {})
+    result = await hass.config_entries.flow.async_init(
+        DOMAIN, context={"source": config_entries.SOURCE_USER}
+    )
+    assert result["type"] == "form"
+    assert result["errors"] == {}
+
+    with patch(
+        "custom_components.generac.config_flow.GeneracApiClient.async_get_data",
+        return_value=True,
+    ), patch(
+        "custom_components.generac.async_setup_entry",
+        return_value=True,
+    ) as mock_setup_entry:
+        result2 = await hass.config_entries.flow.async_configure(
+            result["flow_id"],
+            {
+                "username": "test-username",
+                "password": "test-password",
+            },
+        )
+        await hass.async_block_till_done()
+
+    assert result2["type"] == "create_entry"
+    assert result2["title"] == "test-username"
+    assert result2["data"] == {
+        "username": "test-username",
+        "password": "test-password",
+    }
+    assert len(mock_setup_entry.mock_calls) == 1
