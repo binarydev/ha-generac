33df09c72ac039ebdf08596e6ecdf47ed0039e29
Jose Santiago <jose@binary-dev.com>
2025-07-26 02:51:01 -0400

    Update local API test file to accept a cookie string as well

--- _test_local_access.py
+++ _test_local_access.py
@@ -33,7 +33,7 @@ async def main():
     jar = aiohttp.CookieJar(unsafe=True, quote_cookie=False)
     async with aiohttp.ClientSession(cookie_jar=jar) as session:
         api = GeneracApiClient(
-            session, os.environ["GENERAC_USER"], os.environ["GENERAC_PASS"]
+            session, os.environ.get("GENERAC_USER", ""), os.environ.get("GENERAC_PASS", ""), os.environ.get("GENERAC_SESSION_COOKIE", "")
         )
         await api.login()
         device_data = await api.get_device_data()
